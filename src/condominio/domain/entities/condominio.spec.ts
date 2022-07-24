ca8d7e341fccc5233c80acf592e7f3e5ec50669f
Daniel <danielcvt@gmail.com>
2022-07-24 02:05:47 -0300

    add condominio props getters, test props

--- src/condominio/domain/entities/condominio.spec.ts
+++ src/condominio/domain/entities/condominio.spec.ts
@@ -1,8 +1,23 @@
-import Condominio from "./condominio";
+import { Condominio } from "./condominio";
 
-describe("Condominio Tests", ()=>{
-    test('constructor of condominio', ()=>{
-        const condominio = new Condominio("Arcadia");
-        expect(condominio.name).toBe('Arcadia')
-    })
+describe("Condominio Tests", () => {
+  test("Construtor de Condomínio", () => {
+    //Arrange
+    const props = {
+      nome: "Arcadia",
+      email: "arcadia@email.com",
+      endereco: "Rua X",
+      clienteDesde: new Date("2022-03-01"),
+    };
+    //Act
+    const condominio = new Condominio(props);
+    //Assert
+    expect(condominio.props).toStrictEqual(props)
+    // expect(condominio.nome).toBe("Arcadia");
+    // expect(condominio.email).toBe("arcadia@email.com");
+    // expect(condominio.endereco).toBe("Rua X");
+    // expect(condominio.clienteDesde).toBe(props.clienteDesde);
+    // expect(condominio.cnpj).toBe(undefined);
+    // expect(condominio.telefone).toBe(undefined);
+  });
 });
--- src/condominio/domain/entities/condominio.ts
+++ src/condominio/domain/entities/condominio.ts
@@ -1,5 +1,31 @@
-export default class Condominio {
-    constructor(public name: string){
-        
-    }
+
+export type CondominioProps = {
+  nome: string;
+  email: string;
+  endereco: string;
+  cnpj?: string;
+  telefone?: string;
+  clienteDesde: Date;
+};
+export class Condominio {
+  constructor(public readonly props: CondominioProps) {}
+
+  get nome() {
+    return this.props.nome
+  }
+  get email() {
+    return this.props.email
+  }
+  get endereco() {
+    return this.props.endereco
+  }
+  get cnpj() {
+    return this.props.cnpj
+  }
+  get telefone() {
+    return this.props.telefone
+  }
+  get clienteDesde() {
+    return this.props.clienteDesde
+  }
 }
